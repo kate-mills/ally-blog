72d95c11d896a19f241b91caec1c7f1d06429907
Kate Mills <kate.mills1124@gmail.com>
2021-02-11 12:35:18 -0800

    css

--- src/constants/links.js
+++ src/constants/links.js
@@ -19,11 +19,6 @@ const Links = ({ styleClass, children }) => {
           Newsletter
         </Link>
       </li>
-      <li>
-        <Link to="/post" className="page-link">
-          Post
-        </Link>
-      </li>
     </ul>
   )
 }
