abd6ed0bd88cebe21a5f30a9272217b9423cb309
Kate Mills <kate.mills1124@gmail.com>
2021-02-17 08:22:20 -0800

    sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
       },
     },
   plugins: [
+    `gatsby-plugin-sitemap`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
     `gatsby-remark-images`,
